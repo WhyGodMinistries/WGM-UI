922030e025de1a23bf24ac06c0cb130f9081c87f
Patricia Antlitz <paty.nigro@gmail.com>
2025-02-15 21:39:15 -0500

    setup

--- src/App.js
+++ src/App.js
@@ -9,15 +9,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Why God Ministries</h1>
+        <p style={{marginBottom : '25px'}}>Welcome To:</p>
+        <h1 style={{fontFamily : 'Cinzel', fontWeight : '600'}}>Why God Ministries</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Our page is under construction. Please visit us at:
         </p>
         <div className='social_links]_container'>
-          <a href="https://www.youtube.com/@whygodministries" target="_blank" className="social_link"><img src={YT} alt="Youtube page" className="social_icon"/></a>
-          <a href="https://www.instagram.com/whygodministries/" target="_blank"><img src={ig} alt="Instagram page" className="social_icon"/></a>
-          <a href="https://www.instagram.com/whygodministries/" target="_blank"><img src={email} alt="Email icon to open contact form" className="social_icon"/></a>
+          <a href="https://www.youtube.com/@whygodministries" target="_blank"><img src={YT} alt="Youtube page" className="social_icon"/></a>
+          <a href="https://www.instagram.com/whygodministries/" target="_blank" className="social_link"><img src={ig} alt="Instagram page" className="social_icon"/></a>
+          <a href="https://www.instagram.com/whygodministries/" className="social_link"><img src={email} alt="Email icon to open contact form" className="social_icon"/></a>
         </div>
       </header>
     </div>
